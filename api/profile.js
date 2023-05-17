55ed2a5c168c55ad5551d57e5a596e3ab99d3b29
Demansell <demansell2016@gmail.com>
2023-05-17 11:47:05 -0500

    apis done

---
+++ api/profile.js
@@ -0,0 +1,92 @@
+import { clientCredentials } from '../utils/client';
+
+const endpoint = clientCredentials.databaseURL;
+
+// FIXME:  GET ALL AUTHORS
+const getProfile = (uid) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/profile.json?orderBy="uid"&equalTo="${uid}"`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(Object.values(data)))
+    .catch(reject);
+});
+
+// FIXME: CREATE AUTHOR
+const createProfile = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/profile.json`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+// FIXME: GET SINGLE AUTHOR
+const getSingleProfile = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/profile/${firebaseKey}.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    }, // you technically do not need the options object for GET requests, but using it here for consistency
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data)) // will resolve a single object
+    .catch(reject);
+});
+
+// FIXME: DELETE AUTHOR
+const deleteSingleProfile = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/profile/${firebaseKey}.json`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+// FIXME: UPDATE AUTHOR
+const updateProfile = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/profile/${payload.firebaseKey}.json`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then(resolve)
+    .catch(reject);
+});
+
+// TODO: GET A SINGLE AUTHOR'S BOOKS
+const getProfileExpense = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/expense.json?orderBy="profile_id"&equalTo="${firebaseKey}"`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(Object.values(data)))
+    .catch(reject);
+});
+
+export {
+  getProfile,
+  createProfile,
+  getSingleProfile,
+  deleteSingleProfile,
+  updateProfile,
+  getProfileExpense,
+};
