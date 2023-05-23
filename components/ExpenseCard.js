b7840be1bb99235afb03c5d6417b5f151ec4e94e
Demansell <demansell2016@gmail.com>
2023-05-23 00:42:34 -0500

    got some grammer issues fixed and made it so when adding a expense to profile it goes to expense page still need to fix

--- components/ExpenseCard.js
+++ components/ExpenseCard.js
@@ -18,8 +18,8 @@ function ExpenseCard({ expenseObj, onUpdate }) {
     <Card style={{ width: '18rem', margin: '10px' }}>
       <Card.Body>
         <Card.Title>{expenseObj.title}</Card.Title>
-        <p className="card-text bold">{expenseObj.need && <span>NEED?<br /></span> } ${expenseObj.monthly_total}</p>
-        <p className="card-text bold"> $</p>
+        <p className="card-text bold">{expenseObj.need && <span>Need?<br /></span> } ${expenseObj.monthly_total}</p>
+        <p className="card-text bold"> {expenseObj.profile_id} </p>
         {/* DYNAMIC LINK TO VIEW THE BOOK DETAILS  */}
         <Link href={`/expense/${expenseObj.firebaseKey}`} passHref>
           <Button variant="primary" className="m-2">VIEW</Button>
@@ -43,6 +43,7 @@ ExpenseCard.propTypes = {
     monthly_total: PropTypes.string,
     price: PropTypes.string,
     firebaseKey: PropTypes.string,
+    profile_id: PropTypes.string,
   }).isRequired,
   onUpdate: PropTypes.func.isRequired,
 };
